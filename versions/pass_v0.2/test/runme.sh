35d16590cb5455d75e789858694ee6e154665eb7
Rene <rwarren@bcgsc.ca>
2018-06-18 10:46:41 -0700

    v0.2
    Bug fix (peptide sequences were not input in the reverse order, which resulted in short contigs)
    
    Re-arranged the repo

---
+++ versions/pass_v0.2/test/runme.sh
@@ -0,0 +1,4 @@
+../PASS -f AAreadsLEN6-COV30.fa -m 4 -w 1 -o 1 -b len6
+../PASS -f AAreadsLEN8-COV30.fa -m 4 -w 1 -o 1 -b len8
+../PASS -f AAreadsLEN10-COV30.fa -m 4 -w 1 -o 1 -b len10
+../PASS -f AAreadsLEN12-COV30.fa -m 4 -w 1 -o 1 -b len12
